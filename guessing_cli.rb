3384dd8216f57144eb61abe214a06afb853c4dde
Eyal Toledano <eyaltoledano@me.com>
2019-03-08 14:15:13 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,9 +1,9 @@
 def run_guessing_game
   command = ""
+  number = rand(1..6)
   while command
     puts "Guess a number between 1 and 6."
     command = gets.chomp
-    number = rand(1..6)
     if command == number
       puts "You guessed the correct number!"
     elsif command == "exit"
